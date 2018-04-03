bb1ddc8bc68b947463f76876be3534c0717278ff
Luke <luke.michals@gmail.com>
2018-04-02 19:19:50 -0700

    refactor to separate components into separate files

--- src/components/PasswordForgetPage/PasswordForgetPage.jsx
+++ src/components/PasswordForgetPage/PasswordForgetPage.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 
-// import firebase methods
-import { auth } from "../../firebase";
+// component imports
+import { PasswordForgetForm } from "../";
 
 const PasswordForgetPage = () =>
   <div>
@@ -9,64 +9,4 @@ const PasswordForgetPage = () =>
     <PasswordForgetForm />
   </div>;
 
-const updateByPropertyName = (propertyName, value) => () => ({
-  [propertyName]: value
-});
-
-const INITIAL_STATE = {
-  email: "",
-  error: null
-};
-
-class PasswordForgetForm extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { ...INITIAL_STATE };
-  }
-
-  onSubmit = event => {
-    const { email } = this.state;
-
-    auth
-      .doPasswordReset(email)
-      .then(() => {
-        this.setState(() => ({ ...INITIAL_STATE }));
-      })
-      .catch(error => {
-        this.setState(updateByPropertyName("error", error));
-      });
-
-    event.preventDefault();
-  };
-
-  render() {
-    const { email, error } = this.state;
-
-    const isInvalid = email === "";
-
-    return (
-      <form onSubmit={this.onSubmit}>
-        <input
-          value={this.state.email}
-          onChange={event =>
-            this.setState(updateByPropertyName("email", event.target.value))}
-          type="text"
-          placeholder="Email Address"
-        />
-        <button disabled={isInvalid} type="submit">
-          Reset My Password
-        </button>
-
-        {error &&
-          <p>
-            {error.message}
-          </p>}
-      </form>
-    );
-  }
-}
-
 export default PasswordForgetPage;
-
-export { PasswordForgetForm };
